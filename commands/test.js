79e5e33709d29dc1c07f1c90b6ac7bd34db85176
Wielebny007x <34553547+Wielebny007x@users.noreply.github.com>
2020-08-31 17:06:14 +0200

    Add files via upload

--- commands/test.js
+++ commands/test.js
@@ -1,7 +1,9 @@
+const emotes = require("../emotes.json");
+
 module.exports = {
     name: 'test',
     description: 'testt',
     execute(message, args) {
-        message.channel.send("<:Ahri:749754895233581089> <- AHRI :D");
+        message.channel.send(`${emotes.Ahri}`);
     }
 }
